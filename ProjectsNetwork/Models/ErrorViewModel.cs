1de09f5ad907a5506b3747ffe20fbc7af59dd01e
Alex <avakhno@3-gis.com>
2020-08-23 21:43:32 -0500

    Add project files.

---
+++ ProjectsNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectsNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
