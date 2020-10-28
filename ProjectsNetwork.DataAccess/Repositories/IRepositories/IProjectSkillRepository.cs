18bd44c65a90c004a0d2be4d72a48164b2ebee2d
Arturo Tepale <atepaleag@gmail.com>
2020-10-28 11:57:26 -0500

    User can now add skills from current list

---
+++ ProjectsNetwork.DataAccess/Repositories/IRepositories/IProjectSkillRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjectsNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectsNetwork.DataAccess.Repositories.IRepositories
+{
+    public interface IProjectSkillRepository : IRepository<ProjectSkill>
+    {
+        public void Update(ProjectSkill projectSkill);
+
+    }
+}
