0057d6fc65324fef0622723dc7093c65475b18c4
Arturo Tepale <atepaleag@gmail.com>
2020-11-29 20:24:38 -0600

    Stored credentials to user-secrets

---
+++ ProjectNetwork.Services/IServices/IAuthMessageSenderOptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProjectsNetwork.Services.IServices
+{
+    public interface IAuthMessageSenderOptions
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
